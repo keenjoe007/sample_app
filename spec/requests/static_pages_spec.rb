f1e1badb0f6403337ee2249ad4ddb1003ea56986
takushi_ueda <takushi.ueda@gmail.com>
2013-12-13 16:51:16 +0900

    Add ContactPage

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -34,9 +34,23 @@ describe "Static pages" do
 			expect(page).to have_content('About Us')
 		end
 
-		it "'About Us'というタイトルを持つべきである。" do
+		it "About Usというタイトルを持つべきである。" do
 			visit '/static_pages/about'
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
 		end
 	end
+
+	describe "Contact page" do
+
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+		end
+	end
+
 end
